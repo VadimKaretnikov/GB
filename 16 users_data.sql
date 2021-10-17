ec7fdb5ec58b8a5647119e0374fc72e0111894a1
VadimKaretnikov <89634123+VadimKaretnikov@users.noreply.github.com>
2021-10-17 11:46:51 +0300

    Add files via upload

--- 16 users_data.sql
+++ 16 users_data.sql
@@ -1,2 +1,2 @@
 insert into `kinopoisk`.`users` (email_address, name, gender, birthdate)
-values ('brutaltag@yandex.ru', 'Тагир Билалов', 'M', date '1978-01-16');
+values ('vadim.karetnikov@gmail.com', 'Вадим Каретников', 'M', date '1963-10-22');
